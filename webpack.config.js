45515c8278683fea2a965066cbc9892110122cda
Hannah <HannahHuerter@icloud.com>
2018-08-15 15:42:28 +0200

    completed the reveal-onscroll behaviour

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
-var path = require("path");
-
+const path = require('path');
+ 
 module.exports = {
- entry: "./app/assets/scripts/App.js",
- output: {
-    path: path.resolve(__dirname, "./app/temp/scripts"),
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'App.js',
+        path: path.resolve(__dirname, './app/temp/scripts')
+    } 
+};
+
+
+
+
+
+/*
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: __dirname + "./app/temp/scripts",
     filename: "App.js"
   },
- module: {
-     loaders: [
-    {
-     loaders: "babel-loader",
-     query: {
-       presets: ["es2015"]
-     },
-     test: /\.js$/,
-     exclude: /node_modules/
-    }
-  ]
- }
+  module: {
+    // save "loaders" to "rules"
+    rules: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+         exclude: /[node_modules]/
+      }
+    ]
+  }
 }
+*/
