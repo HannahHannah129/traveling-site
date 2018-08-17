0c2163845b5cdc00761ead0cfe058eacac7b32f8
Hannah <HannahHuerter@icloud.com>
2018-08-17 11:11:00 +0200

    completed modal overlay.

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+
+class Modal {
+  constructor() {
+    this.openModalButton = $(".open-modal");
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal__close");
+    this.events();
+  }
+
+  events() {
+    // clicking the open modal button
+    //with bind ! the current jeyword will keep IT S VALUE!
+    this.openModalButton.click(this.openModal.bind(this));
+
+    // clicking the x close modal button
+    this.closeModalButton.click(this.closeModal.bind(this));
+
+    // pushes any key
+    $(document).keyup(this.keyPressHandler.bind(this));
+
+  }
+    
+// within the event see which key was presst (only if it is the escape key close the modal!)
+keyPressHandler(e) {
+    if (e.keayCode == 27){
+        this.closeModal();
+    }
+}
+
+ 
+
+  openModal() {
+    this.modal.addClass("modal--is-visible");
+    return false;
+  }
+
+  closeModal() {
+    this.modal.removeClass("modal--is-visible");
+  }
+}
+
+export default Modal;
